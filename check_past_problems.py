f342d81c9e2a1fc3eb0095a79b1e07f69d2795ed
Deyao Chen <chendeyao000@gmail.com>
2022-08-20 11:47:40 +0800

    add problem checks

---
+++ check_past_problems.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+with open('past-problems.json') as f:
+  problems = json.load(f)
+
+for problem in problems:
+  assert('name' in problem)
+  assert('slug' in problem)
+  assert('date' in problem)
+  assert('difficulty' in problem)
+  with open(f'{os.path.join(problem["slug"], "statement.json")}') as f:
+    statement = json.load(f)
+    assert statement["difficulty"] == problem["difficulty"], f"difficulties in index and in problem are not the same for {problem['slug']}"
+
+print("All checks passed.")
