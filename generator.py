98b17cdef8eda682ef002e50c18e1e1491ef2f9b
Deyao Chen <chendeyao000@gmail.com>
2022-09-12 16:47:51 +0100

    drop problem

--- generator.py
+++ generator.py
@@ -4,104 +4,56 @@ import random
 
 from itertools import accumulate
 from math import gcd
-
-letters = [chr(i) for i in range(97, 97 + 26)]
-
-def blackbox(s, t):
-  dic = {}
-  for i in range(len(s)):
-    dic[s[i]] = 0
-  for i in range(len(s)):
-    dic[s[i]] += 1
-  for i in range(len(t)):
-    dic[t[i]] -= 1
-
-  li = []
-  for k, v in dic.items():
-    li.append([k, v])
-  li.sort()
-  li = [el for el in li if el[1] != 0]
-  li = [el[0] + ' ' + str(el[1]) for el in li]
-  return li
-
+from collections import deque
 
 def main():
 
   test_cases = []
 
-  for _ in range(40):
+  for _ in range(50):
+    n = random.randint(2, 15)
+    m = random.randint(2, 15)
 
-    s = []
-    n = random.randint(1, 1000000)
-    m = random.randint(1, n)
+    links = []
+    links_memo = set()
+    for i in range(m):
+      a = random.randint(1, n)
+      b = random.randint(1, n)
+      if a > b:
+        a, b = b, a
+      if (a, b) not in links_memo and a != b:
+        links.append([str(a), str(b)])
+        links_memo.add((a, b))
+    m = len(links)
+
+    IN = str(n) + ' ' + str(m) + '\n'
+    for el in links:
+      IN += el[0] + ' ' + el[1] + '\n'
+
+    links = [[int(el[0]) - 1, int(el[1]) - 1] for el in links]
+
+    deg = {}
     for i in range(n):
-      s.append(random.choice(letters))
-    t = random.sample(s, k = m)
+      deg[i] = 0
+    for link in links:
+      deg[link[0]] += 1
+      deg[link[1]] += 1
 
-    s = ''.join(s)
-    t = ''.join(t)
+    cnt = 0
+    for k, v in deg.items():
+      if v % 2 == 1:
+        cnt += 1
 
-    IN = s + '\n' + t + '\n'
-    OUT = ''
-    for el in blackbox(s, t):
-      OUT += el + '\n'
+    if cnt == 0 or cnt == 2:
+      OUT = 'Yes'
+    else:
+      OUT = 'No'
 
-    if OUT == '':
-      OUT += '\n'
 
     test_cases.append({
       "input": f"{IN}",
       "output": f"{OUT}"
-    })
-
-  n = 1000000
-  m = 911444
-  s = []
-  for i in range(n):
-    s.append(random.choice(letters))
-  t = random.sample(s, k = m)
-
-  s = ''.join(s)
-  t = ''.join(t)
-
-  IN = IN = s + '\n' + t + '\n'
-  OUT = ''
-  for el in blackbox(s, t):
-    OUT += el + '\n'
-
-  if OUT == '':
-    OUT += '\n'
-
-  test_cases.append({
-    "input": f"{IN}",
-    "output": f"{OUT}"
-  })
-
-  IN = 'edgecase/nedgecase'
-  OUT = '\n'
-  test_cases.append({
-    "input": f"{IN}",
-    "output": f"{OUT}"
-  })
-
-
-
-  # for _ in range(30):
-
-  #   n = random.randint(100, 100000)
-  #   a = []
-  #   for i in range(n):
-  #     a.append(str(random.randint(0, 100000)))
-  #   IN = str(n) + '\n' + ' '.join(a) + '\n'
-  #   a = [int(el) % 10 for el in a]
-  #   OUT = str(blackbox(n, a)) + '\n'
-
-
-  #   test_cases.append({
-  #     "input": f"{IN}",
-  #     "output": f"{OUT}",
-  #     "subtask": 2
-  #   })
+    }) 
 
   
   print(json.dumps(test_cases, indent=2))
