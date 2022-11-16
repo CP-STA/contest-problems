9264be350461954b36863f73c77eedd3c0776db5
Deyao Chen <chendeyao000@gmail.com>
2022-11-16 17:32:16 +0000

    updates

--- generator.py
+++ generator.py
@@ -5,71 +5,70 @@ from itertools import accumulate
 from math import gcd
 from collections import deque
 
-def solve(n, li):
-    book = [0 for _ in range(n)]
-    for el in li:
-        l = el[0]
-        r = el[1]
-        l -= 1
-        r -= 1
+import heapq
+def dijkstra(graph, N, s):
+    dist = [float('inf') for _ in range(N)]
+    dist[s] = 0
 
-        book[l] += 1
-        if r != n - 1:
-            book[r + 1] -= 1
-
-    book = list(accumulate(book))
-
-    cnt = sum(book[i] == 0 for i in range(n))
-    return cnt
+    data = [[0, s]]
+    while data:
+        pos = heapq.heappop(data)[1]
+        for node, cost in graph[pos]:
+            if dist[pos] + cost < dist[node]:
+                dist[node] = dist[pos] + cost
+                heapq.heappush(data, [dist[node], node])
+    return dist
 
 def main():
     test_cases = []
-    for _ in range(30):
-        n = random.randint(1, 99)
-        m = random.randint(1, 99)
+    for _ in range(100):
 
-        li = []
+        routes = set()
+
+        n = random.randint(1, 1000)
+        m = random.randint(1, 1000)
+
         for i in range(m):
-            l = random.randint(1, n)
-            r = random.randint(l, n)
-            li.append([l, r])
+            a = random.randint(1, n)
+            b = random.randint(1, n)
+            if a > b:
+                a, b = b, a
+            if a != b and (a, b) not in routes:
+                routes.add((a, b))
 
-        ans = solve(n, li)
+        m = len(routes)
+        
+        network = []
+        for el in routes:
+            a, b = el
+            network.append([a, b, random.randint(1, 100)])
 
-        li = [[str(el[0]), str(el[1])] for el in li]
-        li = [' '.join(el) for el in li]
+        s = random.randint(1, n)
 
-        IN = str(n) + ' ' + str(m) + '\n' + '\n'.join(li)
-        OUT = str(ans)
+        IN = str(n) + ' ' + str(m) + ' ' + str(s) + '\n'
+
+        graph = []
+        for i in range(n):
+            graph.append([])
+        for el in network:
+            a, b, c = el
+
+            graph[a - 1].append([b - 1, c])
+            graph[b - 1].append([a - 1, c])
+            IN += str(a) + ' ' + str(b) + ' ' + str(c) + '\n'
+
+        dist = dijkstra(graph, n, s - 1)
+
+        cnt = 0
+        for d in dist:
+            if d <= 30:
+                cnt += 1
+
+        OUT = str(cnt)
 
         test_cases.append({
         "input": f"{IN}",
-        "output": f"{OUT}",
-        "subtask": 1
-        })
-
-    for _ in range(30):
-        n = random.randint(100, 10 ** 5)
-        m = random.randint(100, 10 ** 5)
-
-        li = []
-        for i in range(m):
-            l = random.randint(1, n)
-            r = random.randint(l, n)
-            li.append([l, r])
-
-        ans = solve(n, li)
-
-        li = [[str(el[0]), str(el[1])] for el in li]
-        li = [' '.join(el) for el in li]
-
-        IN = str(n) + ' ' + str(m) + '\n' + '\n'.join(li)
-        OUT = str(ans)
-
-        test_cases.append({
-        "input": f"{IN}",
-        "output": f"{OUT}",
-        "subtask": 2
+        "output": f"{OUT}"
         })
 
     print(json.dumps(test_cases, indent = 2))
